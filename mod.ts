c0e51a78ec60c91a78eeb6d18b1806b13f722183
denyncrawford <kiquerangelb@gmail.com>
2022-01-05 13:19:38 -0300

    multiplataform support

--- mod.ts
+++ mod.ts
@@ -1,14 +1,26 @@
 import { Plug } from "https://deno.land/x/plug/mod.ts";
 import { join } from 'https://deno.land/std/path/mod.ts'
 import { ensureDir, exists } from 'https://deno.land/std/fs/mod.ts'
-import binary from 'https://x.nest.land/degui2@1.0.0-alpha/binary.b.ts'
+import "https://deno.land/x/dotenv/load.ts";
+
+const libUrls = {
+  linux: {
+    url: Deno.env.get("DEV") == 'true' ? './linux.binary.b.ts' : 'https://x.nest.land/degui2@1.0.1-alpha/linux.binary.b.ts',
+    name: 'libdegui.so'
+  },
+  windows: {
+    url: Deno.env.get("DEV") == 'true' ? './windows.binary.b.ts' : 'https://x.nest.land/degui2@1.0.1-alpha/windows.binary.b.ts',
+    name: 'degui.dll'
+  },
+}
 
 const writeBinary = async (dir: string) => {
   const libDir = join(dir, 'lib');
   const installed = await exists(libDir)
   if (installed) return;
+  const binary = Deno.build.os === 'windows' ? (await import(libUrls.windows.url)).default : (await import(libUrls.linux.url)).default
   await ensureDir(libDir)
-  await Deno.writeFile(join(libDir, 'degui.dll'), binary)
+  await Deno.writeFile(join(libDir, Deno.build.os === 'windows' ? libUrls.windows.name : libUrls.linux.name), binary)
   return libDir
 }
 
