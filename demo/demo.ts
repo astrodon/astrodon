7499f18405aed57c82a34cf7ee52d68fbebbbac6
Marc Espin Sanz <mespinsanz@gmail.com>
2022-01-06 16:12:09 +0100

    :bug: Fixed demo!

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,9 +1,16 @@
 import { App } from "../mod.ts";
 
-const indexPath = await Deno.realPath("./demo/index.html");
+let indexPath = "";
+const isDev = Deno.env.get("DEV") == "true";
+
+if(isDev){
+  indexPath = `file://${await Deno.realPath("./demo/index.html")}`;
+} else {
+  indexPath = 'https://rawcdn.githack.com/astrodon/astrodon/fdf9523e44f78c40290141f0288e0e1b468dc075/demo/index.html'
+}
 
 const windows = [
-  { title: "spaghettis > ravioli", url: `file://${indexPath}` },
+  { title: "spaghettis > ravioli", url: indexPath },
 ];
 
 const app = await App.withWindows(windows);
