51eb40f1e067597dcd1ee3aea480dae15ec5e360
denyncrawford <kiquerangelb@gmail.com>
2022-01-07 02:35:17 -0300

    Vue example enhaced

--- examples/vuejs_app/src/views/About.js
+++ examples/vuejs_app/src/views/About.js
@@ -4,7 +4,7 @@ export default {
   template: `
     <div class="w-full h-full flex flex-col items-center justify-center">
       <h1 class="text-6xl -mt-20">About Page</h1>
-      <p class="mt-10">Some text here</p>
+      <p class="mt-10">https://github.com/astrodon/astrodon</p>
     </div>
   `
 }
--- examples/vuejs_app/utils.ts
+++ examples/vuejs_app/utils.ts
@@ -1,8 +1,10 @@
-export const flat = (obj = {}, res = {}, extraKey = "") => {
+export const flat = (obj: Record<string, Uint8Array>, res?: Record<string, Uint8Array>, extraKey = "") => {
+  if (!res) res = {};
   for (const key in obj) {
     if (typeof obj[key] !== "object" || obj[key] instanceof Uint8Array) {
       res[extraKey + key] = obj[key];
     } else {
+      //@ts-ignore: ts still supposes obj[key] can be an instanceof Uint8Array but it is impossible
       flat(obj[key], res, `${extraKey}${key}/`);
     }
   }
