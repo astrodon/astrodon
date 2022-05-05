40bbe6501154f76f7b35228cf1aeedda976a33d4
marc2332 <mespinsanz@gmail.com>
2022-05-05 22:11:56 +0200

    :green_heart: Fixed astrodon run tests

--- modules/astrodon-cli/tests/resolve_config.test.ts
+++ modules/astrodon-cli/tests/resolve_config.test.ts
@@ -2,29 +2,25 @@ import { DEFAULT_CONFIG, resolveConfiguration } from "../commands/run.ts";
 import { serve } from "https://deno.land/std@0.137.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.137.0/http/file_server.ts";
 import { assertEquals } from "https://deno.land/std@0.137.0/testing/asserts.ts";
-import {
-  dirname,
-  fromFileUrl,
-} from "https://deno.land/std@0.137.0/path/mod.ts";
-const __dirname = dirname(fromFileUrl(import.meta.url));
 
-const port = 3000;
+const PORT = 3000;
 const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const handler = async (req: Request) => {
-  return await serveDir(req, { fsRoot: __dirname });
+  return await serveDir(req, { fsRoot: Deno.cwd() });
 };
 
 Deno.test("Resolve remote configuration", async () => {
   const controller = new AbortController();
-  serve(handler, { port, signal: controller.signal });
-  await delay(3000);
+  serve(handler, { port: PORT, signal: controller.signal });
+  await delay(1000);
   const options = {
-    config: `http://localhost:${port}/astrodon.config.ts`,
+    config: `http://localhost:${PORT}/modules/astrodon-cli/tests/astrodon.config.ts`,
   };
   const config = await resolveConfiguration(options);
   controller.abort();
   assertEquals(config?.name, DEFAULT_CONFIG.name);
+  await delay(1000);
 });
 
 Deno.test({
